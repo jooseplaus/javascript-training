0cc752123d5c8cf78942dc88ac8e3e29fc397e8c
jooseplaus <lausjoosep@gmail.com>
2024-10-16 21:45:32 +0300

    Soovin näha kas uuendused tulevad läbi siia faili

--- java-training-1.js
+++ java-training-1.js
@@ -12,3 +12,35 @@ function greetUser(name) {
 
 // Funktsiooni testimine
 console.log(greetUser("Joosep"));
+
+
+// Funktsioon, mis tervitab kasutajat
+function greetUser(name) {
+    // Saame hetkelise kuupäeva
+    const today = new Date();
+    
+    // Kuupäeva formaat: Päev-Kuu-Aasta
+    const dateString = today.toLocaleDateString();
+
+    // Tervitame kasutajat
+    return `Tere, ${name}! Täna on ${dateString}.`;
+}
+
+// Funktsiooni testimine
+console.log(greetUser("Joosep"));
+
+
+// Funktsioon, mis tervitab kasutajat
+function greetUser(name) {
+    // Saame hetkelise kuupäeva
+    const today = new Date();
+    
+    // Kuupäeva formaat: Päev-Kuu-Aasta
+    const dateString = today.toLocaleDateString();
+
+    // Tervitame kasutajat
+    return `Tere, ${name}! Täna on ${dateString}.`;
+}
+
+// Funktsiooni testimine
+console.log(greetUser("Joosep"));
