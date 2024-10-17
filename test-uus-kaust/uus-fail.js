197a919815c413d4962c9febda303f2a6fdcea91
jooseplaus <152327405+jooseplaus@users.noreply.github.com>
2024-10-17 11:30:36 +0300

    Update uus-fail.js
    Lisatud uus rida ja uus koht!

--- test-uus-kaust/uus-fail.js
+++ test-uus-kaust/uus-fail.js
@@ -9,3 +9,16 @@ function greetUser(name) {
     // Tervitame kasutajat
     return `Tere, ${name}! Täna on ${dateString}.`;
 }
+// Funktsioon, mis tervitab kasutajat
+function greetUser(name) {
+    // Saame hetkelise kuupäeva
+    const today = new Date();
+    
+    // Kuupäeva formaat: Päev-Kuu-Aasta
+    const dateString = today.toLocaleDateString();
+
+    // Tervitame kasutajat
+    return `Tere, ${name}! Täna on ${dateString}.`;
+}
+
+//Lisasin siia lihtsalt uue rea, et testida seda kuidas ma nüüd selle enda arvuti tõmban. 
